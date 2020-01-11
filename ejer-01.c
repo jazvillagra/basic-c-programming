3d5c1e333c42116369a173b7da00420495b1fa17
Jazmin Villagra <jvillagra@sodep.com.py>
2020-01-11 14:06:13 -0300

    First class

---
+++ ejer-01.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void) {
+
+	int dia, mes, anio;
+	printf("Ingrese dia:");
+	scanf("%d",&dia);
+	printf("Ingrese mes:");
+	scanf("%d",&mes);
+	printf("Ingrese anio:");
+	scanf("%d",&anio);
+	printf("La fecha ingresada es: %d/%d/%d",dia, mes, anio);
+	
+	return 0;
+}
